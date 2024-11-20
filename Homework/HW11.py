a73ca77ee38dfeb0cf536bead638fa912ee94e24
trevorswan11 <tcs94@case.edu>
2024-11-20 18:20:59 -0500

    updated code

---
+++ Homework/HW11.py
@@ -0,0 +1,15 @@
+inside = 15 # C
+outside = 35 # C
+dT = outside - inside # temperature
+
+t_12 = 0.01 # m
+t_23 = 0.08 # m
+t_34 = 0.10 # m
+t_tot = t_12 + t_23 + t_34
+
+L_a = 0.60 # m
+L_b = 0.005 # m
+L_tot = L_a + L_b
+
+A_c = L_tot * t_tot # m^2
+
