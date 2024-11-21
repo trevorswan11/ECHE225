8af6c4e50c2ff2f9256561ad489f8d5588974e5f
Trevor Swan <tcs94@case.edu>
2024-11-20 19:22:37 -0500

    updated code

--- Homework/HW11.py
+++ Homework/HW11.py
@@ -1,15 +1,22 @@
-inside = 15 # C
+inside = 20 # C
 outside = 35 # C
 dT = outside - inside # temperature
 
+# Gypsum
 t_12 = 0.01 # m
-t_23 = 0.08 # m
 t_34 = 0.10 # m
-t_tot = t_12 + t_23 + t_34
+
+# Brick
+
+
+# Insulation
+k = 0.03 # W/mK
+t_23 = 0.08 # m
 
 L_a = 0.60 # m
 L_b = 0.005 # m
+
 L_tot = L_a + L_b
 
+t_tot = t_12 + t_23 + t_34
 A_c = L_tot * t_tot # m^2
-
