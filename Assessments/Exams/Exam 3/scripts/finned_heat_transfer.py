b25af42e3bc726ef68cecb2624bf7e03eb5caf8d
trevorswan11 <tcs94@case.edu>
2024-12-06 01:15:01 -0500

    i think were ready

---
+++ Assessments/Exams/Exam 3/scripts/finned_heat_transfer.py
@@ -0,0 +1,55 @@
+from math import cosh, tanh, sqrt, exp
+
+def calculate_m_parameter(h: float, P: float, k: float, A_c: float) -> float:
+    return sqrt((h * P) / (k * A_c))
+
+def calculate_T_of_x_infinite(h: float, 
+                     P: float, 
+                     k: float, 
+                     A_c: float, 
+                     x: float, 
+                     T_b: float, 
+                     T_inf: float) -> float:
+    m = calculate_m_parameter(h, P, k, A_c)
+    exponential: float = exp(-1 * m * x)
+    T_diff: float = T_b - T_inf
+    return exponential * T_diff + T_inf
+
+def calculate_T_of_x_adiabatic(h: float, 
+                     P: float, 
+                     k: float, 
+                     A_c: float, 
+                     x: float, 
+                     L: float,
+                     T_b: float, 
+                     T_inf: float) -> float:
+    m = calculate_m_parameter(h, P, k, A_c)
+    numerator: float = cosh(m * (L - x))
+    denominator: float = cosh(m * L)
+    T_diff: float = T_b - T_inf
+    return (numerator / denominator) * T_diff + T_inf
+
+def calculate_Q_infinite(h: float, 
+                     P: float, 
+                     k: float, 
+                     A_c: float, 
+                     T_b: float, 
+                     T_inf: float) -> float:
+    square_root: float = sqrt(h * P * k, A_c)
+    T_diff: float = T_b - T_inf
+    return square_root * T_diff
+
+def calculate_Q_adiabatic(h: float, 
+                     P: float, 
+                     k: float, 
+                     A_c: float, 
+                     L: float,
+                     T_b: float, 
+                     T_inf: float) -> float:
+    m = calculate_m_parameter(h, P, k, A_c)
+    square_root = sqrt(h * P * k * A_c)
+    T_diff = T_b - T_inf
+    hyperbolic = tanh(m * L)
+    return square_root * T_diff * hyperbolic
+
+
---
+++ Assessments/Exams/Exam 3/scripts/simple_conduction.py
@@ -0,0 +1,36 @@
+from typing import List
+from math import log, exp, pi
+
+def conductive_resistance(L: float, k: float, A_c: float) -> float:
+    return L / (k * A_c)
+
+def conductive_resistance_cyl(r_1: float, r_2: float, L: float, K: float) -> float:
+    numerator: float = log(r_2 / r_1)
+    denominator: float = log(2 * pi * L * K)
+    return numerator / denominator
+
+def conductive_resistance_sph(r_1: float, r_2: float, K: float) -> float:
+    numerator: float = r_2 - r_1
+    denominator: float = 4 * pi * r_1 * r_2 * K
+    return numerator / denominator
+
+def convective_resistance(h: float, A_s: float) -> float:
+    return 1 / (h * A_s)
+
+def series_resistance(resistances: List[float]) -> float:
+    return sum(resistances)
+
+def parallel_resistances(resistances: List[float]) -> float:
+    summ: List[float] = []
+    for resistance in resistances:
+        summ.append(1 / resistance)
+    return 1 / sum(summ)
+
+def calculate_Q_conduction(T_1: float, T_2: float, R_conduction: float) -> float:
+    return (T_1 - T_2) / R_conduction
+
+def calculate_Q_convection(T_1: float, T_2: float, R_convection: float) -> float:
+    return (T_1 - T_2) / R_convection
+
+def calculate_Q_series(T_first: float, T_last: float, R_total: float) -> float:
+    return (T_first - T_last) / R_total 
